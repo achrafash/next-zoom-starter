bef90742d284a08422bec7544401332d3e670f2d
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2020-12-05 12:38:17 +0100

    Initial setup: Typescript, eslint, prettier

---
+++ src/components/ZoomWindow.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+const ZoomWindow: FC = () => {
+    return (
+        <div>
+            <iframe src='' frameBorder='0'></iframe>
+        </div>
+    )
+}
+
+export default ZoomWindow
